3d3d05f10492007eaf3dbf1292d5384a1e8a7179
kedianaman <nkedia@usc.edu>
2017-11-12 00:38:46 -0800

    Reading in Contact and transforming it into a VCard QR Code.

--- AddMe/ViewController.swift
+++ AddMe/ViewController.swift
@@ -7,17 +7,93 @@
 //
 
 import UIKit
+import Contacts
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var qrCodeImageView: UIImageView!
+    var contactStore: CNContactStore!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        contactStore = CNContactStore()
+        fetchContacts()
+        
+        let contact = CNMutableContact()
+        contact.imageData = Data() // The profile picture as a NSData object
+        
+        contact.givenName = "John"
+        contact.familyName = "Appleseed"
+        
+        let homeEmail = CNLabeledValue(label:CNLabelHome, value:"john@example.com" as NSString)
+        let workEmail = CNLabeledValue(label:CNLabelWork, value:"j.appleseed@icloud.com" as NSString)
+        
+        contact.emailAddresses = [homeEmail, workEmail]
+        
+        contact.phoneNumbers = [CNLabeledValue(
+            label:CNLabelPhoneNumberiPhone,
+            value:CNPhoneNumber(stringValue:"(408) 555-0126"))]
+        
+        let homeAddress = CNMutablePostalAddress()
+        homeAddress.street = "1 Infinite Loop"
+        homeAddress.city = "Cupertino"
+        homeAddress.state = "CA"
+        homeAddress.postalCode = "95014"
+        contact.postalAddresses = [CNLabeledValue(label:CNLabelHome, value:homeAddress)]
+        
+        var birthday = DateComponents()
+        birthday.day = 1
+        birthday.month = 4
+        birthday.year = 1988  // You can omit the year value for a yearless birthday
+        contact.birthday = birthday
+        let fb = CNSocialProfile(urlString: "https://www.facebook.com/naman.kedia.5", username: "naman.kedia5", userIdentifier: "", service: "Facebook")
+        let facebookLabeledValue = CNLabeledValue(label: "Facebook", value: fb)
+        contact.socialProfiles = [facebookLabeledValue]
+//        contactToVCard(contact: contact)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func saveContact(contact: CNMutableContact) {
+        let saveRequest = CNSaveRequest()
+        saveRequest.add(contact, toContainerWithIdentifier:nil)
+        try! contactStore.execute(saveRequest)
+    }
+    
+    func fetchContacts() {
+        let predicate: NSPredicate = CNContact.predicateForContacts(matchingName: "Shubham Kedia")
+        let keysToFetch = [CNContactVCardSerialization.descriptorForRequiredKeys()]
+        let contacts = try? contactStore.unifiedContacts(matching: predicate, keysToFetch: keysToFetch as [CNKeyDescriptor])
+        if let contacts = contacts {
+            let contact = contacts.first
+            contactToVCard(contact: contact!)
+        }
+    }
+    
+    func contactToVCard(contact: CNContact) {
+        do {
+            let data = try CNContactVCardSerialization.data(with: [contact])
+            print(data.description)
+            if let filter = CIFilter(name: "CIQRCodeGenerator") {
+                filter.setValue(data, forKey: "inputMessage")
+                let transform = CGAffineTransform(scaleX: 1, y: 1)
+                if let output = filter.outputImage?.transformed(by: transform) {
+                    let image =  UIImage(ciImage: output)
+                    qrCodeImageView.image = image
+                }
+            }
+            
+            if let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                
+                let fileURL = directoryURL.appendingPathComponent("john.appleseed").appendingPathExtension("vcf")
+                do {
+                    try data.write(to: fileURL)
+                } catch {
+                    print("error writing to file")
+                }
+            }
+            
+        } catch {
+            print("error turning into Vcard")
+        }
     }
 
 
